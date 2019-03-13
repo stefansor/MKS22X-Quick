d28d0e78ab31d2594ca7be1b43b823da9c00929d
stefansor <ssorobay00@stuy.edu>
2019-03-13 14:48:35 -0400

    quicksort works must still optimize partition with necessary 2 steps

--- Quick.java
+++ Quick.java
@@ -5,6 +5,23 @@ public class Quick{
   /*return the value that is the kth smallest value of the array.
   //should make better by not randomizing the number
  */
+
+ public static void quicksort(int[] data){
+   qsorth(data, 0, data.length - 1);
+ }
+
+public static void qsorth(int[] data, int lo, int hi){
+  if(lo >= hi){
+    return;
+  }
+  int pivot = partition(data, lo, hi);
+  qsorth(data, lo, pivot - 1);
+  qsorth(data, pivot + 1, hi);
+}
+
+
+
+
  public static int quickselect(int []data, int k){
    int end = data.length - 1;
    return qsh(data, k, 0, end);
@@ -62,17 +79,19 @@ public class Quick{
 
 
  public static void main(String[] args){
-   int[] yea = new int[]{
-     4, 3, 5, 6, 9, 10, 34, 8, 7, 20 , 23, 13, 45, 40, 2
-   };
-   int[] nah = new int[]{
-     1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0
-   };
-   //System.out.println(Arrays.toString(nah));
-   //System.out.println(partition(nah, 0, nah.length - 1));
-   //System.out.println(Arrays.toString(nah));
-   System.out.println(Arrays.toString(nah));
-   System.out.println(quickselect(nah, 5));
-   System.out.println(Arrays.toString(nah));
+   Random rng = new Random();
+   int[] yea = new int[200000];
+   int[] yea2 = new int[200000];
+   for(int i = 0; i < yea.length; i++){
+     int a = rng.nextInt();
+     yea[i] = a;
+     yea2[i] = a;
+   }
+   quicksort(yea);
+   Arrays.sort(yea2);
+   if(Arrays.equals(yea, yea2)){
+     System.out.println("true");
+   }
+   System.out.println("false");
  }
 }
